df05d5a93fa9d9af9ee06a33b401b783572088bc
xllifi <unicoded@ya.ru>
2026-03-12 21:28:58 +1000

    Admin command now collects instead of equips

--- src/main/kotlin/ru/xllifi/rewards/commands/admin/AdminCosmeticsCommands.kt
+++ src/main/kotlin/ru/xllifi/rewards/commands/admin/AdminCosmeticsCommands.kt
@@ -10,20 +10,37 @@ import ru.xllifi.rewards.commands.Command
 import ru.xllifi.rewards.cosmetic.commands.cosmeticArgument
 import ru.xllifi.rewards.cosmetic.commands.cosmeticKindArgument
 import net.minecraft.network.chat.Component
+import org.jetbrains.exposed.v1.core.dao.id.CompositeID
+import org.jetbrains.exposed.v1.jdbc.transactions.transaction
+import ru.xllifi.rewards.Main
 import ru.xllifi.rewards.cosmetic.commands.getCosmeticArgument
+import ru.xllifi.rewards.cosmetic.sql.CollectedCosmetic
+import ru.xllifi.rewards.cosmetic.sql.CollectedCosmeticsTable
 
 object AdminCosmeticsCommands : Command {
   fun setCollected(ctx: CommandContext<CommandSourceStack>): Int {
     val player = EntityArgument.getPlayer(ctx, "player")
     val (cosmeticKind, cosmetic) = ctx.getCosmeticArgument("cosmetic_kind", "cosmetic")
-    val isEquipped = BoolArgumentType.getBool(ctx, "is_equipped")
-    cosmetic.updateOrCreateFor(player, isEquipped)
+    val isCollected = BoolArgumentType.getBool(ctx, "is_collected")
+    if (isCollected) {
+      cosmetic.updateOrCreateFor(player, null)
+    } else {
+      transaction(Main.database) {
+        CollectedCosmetic.findById(
+          CompositeID {
+            it[CollectedCosmeticsTable.playerUuid] = player.uuid
+            it[CollectedCosmeticsTable.cosmeticKind] = cosmeticKind
+            it[CollectedCosmeticsTable.cosmeticId] = cosmetic.id
+          }
+        )?.delete()
+      }
+    }
     ctx.source.sendSystemMessage(
       Component.translatable(
         "command.rewards.cosmetic.set_collected.success",
         player.displayName,
         Component.literal("${cosmeticKind.snakeCaseName()}:${cosmetic.id}").withStyle(ChatFormatting.YELLOW),
-        Component.translatable("command.rewards.cosmetic.set_collected.success.to_$isEquipped")
+        Component.translatable("command.rewards.cosmetic.set_collected.success.to_$isCollected")
       )
     )
     return Command.SINGLE_SUCCESS
@@ -34,7 +51,7 @@ object AdminCosmeticsCommands : Command {
       argument("player", EntityArgument.player()) {
         cosmeticKindArgument("cosmetic_kind") {
           cosmeticArgument("cosmetic_kind", "cosmetic") {
-            argument("is_equipped", BoolArgumentType.bool()) {
+            argument("is_collected", BoolArgumentType.bool()) {
               executes { ctx -> setCollected(ctx) }
             }
           }
